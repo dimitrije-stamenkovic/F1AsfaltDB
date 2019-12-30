e02a78300b800ccad3420893ba012c4d966f3d9c
mita99796 <muskitos.97@gmail.com>
2019-12-30 14:28:37 +0100

    Detail pages finished

--- ui/src/components/Circuit.js
+++ ui/src/components/Circuit.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { Container, Grid, Image } from "semantic-ui-react";
 
-const Circuit = () => {
-  return <div></div>;
+const Circuit = ({ location }) => {
+  const { state } = location;
+  const { item } = state;
+  return (
+    <Container>
+      {console.log(item)}
+      <Grid>
+        <Grid.Column width={6}>
+          <Image src={item.url} size="big" />
+        </Grid.Column>
+        <Grid.Column width={10}>
+          <div className="circuit-info">
+            <p>Name : {item.circuitName}</p>
+            <p>Contry : {item.country}</p>
+            <p>Length : {item.length}</p>
+          </div>
+        </Grid.Column>
+      </Grid>
+    </Container>
+  );
 };
 
 export default Circuit;
--- ui/src/components/Constructor.js
+++ ui/src/components/Constructor.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { Container, Grid, Image } from "semantic-ui-react";
 
-const Constructor = () => {
-  return <div></div>;
+const Constructor = ({ location }) => {
+  const { state } = location;
+  const { item } = state;
+  return (
+    <Container>
+      {console.log(item)}
+      <Grid>
+        <Grid.Column width={6}>
+          <Image src={item.url} size="big" />
+        </Grid.Column>
+        <Grid.Column width={10}>
+          <div className="driver-info">
+            <p>Name : {item.name}</p>
+            <p>Chassis: {item.chassis}</p>
+            <p>Nationality : {item.nationality}</p>
+          </div>
+        </Grid.Column>
+      </Grid>
+    </Container>
+  );
 };
 
 export default Constructor;
