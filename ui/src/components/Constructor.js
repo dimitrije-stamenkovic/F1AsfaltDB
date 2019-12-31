f6ebe7d5fda11941eb5d6364226ce054d28083fb
mita99796 <muskitos.97@gmail.com>
2019-12-31 15:29:13 +0100

    SimiliarConstructors finished

--- ui/src/components/Constructor.js
+++ ui/src/components/Constructor.js
@@ -1,24 +1,34 @@
 import React from "react";
 import { Container, Grid, Image } from "semantic-ui-react";
 import "./Constructor.css";
+import SimiliarConstructors from "./SimiliarComponents/SimiliarConstructors";
 
 const Constructor = ({ location }) => {
   const { state } = location;
   const { item } = state;
   return (
     <Container className="constructor">
-      {console.log(item)}
-      <Grid>
-        <Grid.Column width={6}>
-          <Image src={item.url} size="big" />
-        </Grid.Column>
-        <Grid.Column width={10}>
-          <div className="constructor-info">
-            <p>Name : {item.name}</p>
-            <p>Chassis: {item.chassis}</p>
-            <p>Nationality : {item.nationality}</p>
-          </div>
-        </Grid.Column>
+      <Grid divided="vertically">
+        <Grid.Row columns={2}>
+          <Grid.Column width={6}>
+            <Image src={item.url} size="big" />
+          </Grid.Column>
+          <Grid.Column width={10}>
+            <div className="constructor-info">
+              <p>Name : {item.name}</p>
+              <p>Chassis: {item.chassis}</p>
+              <p>Nationality : {item.nationality}</p>
+            </div>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+          <SimiliarConstructors
+            constructor={{
+              nationality: item.nationality,
+              name: item.name
+            }}
+          />
+        </Grid.Row>
       </Grid>
     </Container>
   );
--- ui/src/components/SimiliarComponents/SimiliarConstructors.js
+++ ui/src/components/SimiliarComponents/SimiliarConstructors.js
@@ -6,27 +6,27 @@ import { Card, Dimmer, Loader } from "semantic-ui-react";
 import { Link } from "@reach/router";
 
 const GET_CONSTRUCTORS = gql`
-  {
-    Circuit(filter: { AND: [{ length_gt: 5.3 }, { length_lt: 5.6 }] }) {
-      circuitId
+  query GET_CIRCUITS($nationality: String, $name: String) {
+    Constructor(
+      filter: { AND: [{ nationality: $nationality }, { name_not: $name }] }
+    ) {
+      constructorId
       url
-      circuitName
-      country
-      length
+      name
+      chassis
+      nationality
     }
   }
 `;
 
-const SimiliarConstructors = () => {
+const SimiliarConstructors = ({ constructor }) => {
   const [activeItemIndex, setActiveItemIndex] = useState(0);
   const chevronWidth = 40;
 
   const { loading, error, data } = useQuery(GET_CONSTRUCTORS, {
     variables: {
-      wins: driver.wins,
-      podiums: driver.podiums,
-      nationality: driver.nationality,
-      name: driver.name
+      nationality: constructor.nationality,
+      name: constructor.name
     }
   });
 
@@ -39,37 +39,31 @@ const SimiliarConstructors = () => {
   if (error) return `Error! ${error}`;
   return (
     <div style={{ padding: `0 ${chevronWidth}px` }}>
-      <ItemsCarousel
-        infiniteLoop
-        requestToChangeActive={setActiveItemIndex}
-        activeItemIndex={activeItemIndex}
-        numberOfCards={3}
-        gutter={20}
-        leftChevron={<button>{"<"}</button>}
-        rightChevron={<button>{">"}</button>}
-        outsideChevron
-        chevronWidth={chevronWidth}
-      >
-        {data.Driver.map(item => (
-          <Link
-            key={item.givenName}
-            to={"/drivers/" + item.driverId}
-            state={{ item }}
-          >
-            <Card style={{ marginLeft: 20, marginTop: 20 }}>
-              <img src={item.url} height={300} />
-              <Card.Content>
-                <Card.Header>{item.givenName}</Card.Header>
-                <Card.Meta>
-                  <span>{item.givenName}</span>
-                  <span>{item.familyName}</span>
-                  <span>{item.permanentNumber}</span>
-                </Card.Meta>
-              </Card.Content>
-            </Card>
-          </Link>
-        ))}
-      </ItemsCarousel>
+      {data.Constructor.length === 0 ? (
+        "There is no constructors with same nationality"
+      ) : (
+        <div>
+          <Card.Group>
+            {data.Constructor.map(item => (
+              <Link
+                key={item.name}
+                to={"/constructors/" + item.constructorId}
+                state={{ item }}
+              >
+                <Card style={{ marginLeft: 20, marginTop: 20 }}>
+                  <img src={item.url} height={300} />
+                  <Card.Content>
+                    <Card.Header>{item.name}</Card.Header>
+                    <Card.Meta>
+                      <span>{item.nationality}</span>
+                    </Card.Meta>
+                  </Card.Content>
+                </Card>
+              </Link>
+            ))}
+          </Card.Group>
+        </div>
+      )}
     </div>
   );
 };
